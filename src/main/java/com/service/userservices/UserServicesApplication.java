d1348d847fcd9e80f0a6bc7b93b44b949516d6ac
anggiat0304 <anggiatpangaribuan28@gmail.com>
2023-03-03 19:21:27 +0700

    Configuration Maven

--- src/main/java/com/service/userservices/UserServicesApplication.java
+++ src/main/java/com/service/userservices/UserServicesApplication.java
@@ -2,9 +2,14 @@ package com.service.userservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class UserServicesApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserServicesApplication.class, args);
